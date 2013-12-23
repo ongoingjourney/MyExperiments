0b2b64e046edb18e43697c256e81db1b8365cc94
San <ongoingjourney@outlook.com>
2013-12-22 23:01:42 -0800

    Cleaning up

--- InterviewStuff/src/com/sanjeeb/java/jmock/RemoteService.java
+++
@@ -1,7 +0,0 @@
-package com.sanjeeb.java.jmock;
-
-public interface RemoteService {
-	public int addition(int a, int b);
-	public int subtraction(int a, int b);
-	public int multiplication(int a, int b);
-}
--- InterviewStuff/src/com/sanjeeb/java/jmock/RemoteServiceJMockTest.java
+++
@@ -1,6 +0,0 @@
-package com.sanjeeb.java.jmock;
-
-
-public class RemoteServiceJMockTest extends TestC{
-
-}
